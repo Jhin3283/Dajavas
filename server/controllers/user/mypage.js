8bb6057dbddf53758fe82e1dc6900204874434a0
Boyoung <boyoung.y589@gmail.com>
2022-03-02 17:52:10 +0900

    MERGE: All

--- server/controllers/user/mypage.js
+++ server/controllers/user/mypage.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   put: async (req, res) => {
     // 개인정보 수정
-    console.log('요청된 바디',req)
+    console.log('요청된 바디',req.body)
     const { nickname, password, email } = req.body;
     const validate = await func.validateToken(req.headers.authorizationtoken);
     // const userInfo = await func.checkUser(req.query.email);
