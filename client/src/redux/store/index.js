985e69247c0d6715230cbfa01f19cfa300742f3d
Jhin3283 <hj3283@gmail.com>
2022-04-04 17:12:26 +0900

    FIX

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,25 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 //import reducers from "./reducers";
-import thunk from 'redux-thunk' // 텅크를 사용하는이유: action에서 dispatch를 가능하게해준다.
-import logger from 'redux-logger'
-import rootReducer from '../store/reducers/index'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from "redux-thunk"; // 텅크를 사용하는이유: action에서 dispatch를 가능하게해준다.
+import logger from "redux-logger";
+import rootReducer from "../store/reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const middleware = [logger, thunk]
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)))
+const middleware = [logger, thunk];
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-
-<<<<<<< HEAD
-// /* eslint-disable no-underscore-dangle */
-// const store = createStore(
-//   reducers,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // Redux DevTools Extension
-//   /* applyMiddleware(...middleware) */
-// );
-// /* eslint-enable */
-
-=======
->>>>>>> 802cf51347c59628a478503963b978a8e98bf478
 export default store;
-
-
--- client/src/redux/store/reducers/updateFishReducer/updateFishReducer.js
+++ client/src/redux/store/reducers/updateFishReducer/updateFishReducer.js
@@ -1,28 +1,18 @@
-<<<<<<< HEAD
-import { updateFish } from '../../actions'
-import { TARGET_FIND, UPDATE_FISH } from '../../actions/actionTypes'
-=======
-import { TARGET_FIND} from '../../actions/actionTypes'
->>>>>>> 802cf51347c59628a478503963b978a8e98bf478
+import { TARGET_FIND } from "../../actions/actionTypes";
 
 const initialState = {
+  data: null,
+};
 
-    data: null
-   
-}
-
-const updateFishReducer = (state=initialState, action) => {
-    switch(action.type) {
-        case TARGET_FIND:
-            return {
-                ...state,
-                data: action.payload
-            }
-<<<<<<< HEAD
-        default: return state  
-=======
-        default: return state   
->>>>>>> 802cf51347c59628a478503963b978a8e98bf478
-    }
-}
-export default updateFishReducer
+const updateFishReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TARGET_FIND:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+export default updateFishReducer;
